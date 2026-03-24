d29da338cc55720ad9e14cf4c9018aad853b5852
Yuri Santana <yuri03ysl@gmail.com>
2026-03-23 21:54:00 -0300

    Starting

---
+++ codeforces/nextRound.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n,k,approved,threshold;
+    approved = threshold = 0;
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+
+
+
+
+    }
+
+
+    return 0;
+}
